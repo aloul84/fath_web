768f51eb690bd94babd513680ed748ca04fb1f2a
aloul84 <aloul84@gmail.com>
2025-08-24 09:48:17 +0000

    createWebHashHistory

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import CountriesView from '@/views/CountriesView.vue'
 import HomeView from '@/views/HomeView.vue'
 import AnimalsView from '@/views/AnimalsView.vue'
@@ -52,7 +52,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
